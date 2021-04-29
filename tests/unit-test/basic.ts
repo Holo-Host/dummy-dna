e32a34178ed18c8022a23d7c5b9f371cd607bc2b
jetttech <ldjetton21@gmail.com>
2021-04-28 20:13:25 -0500

    add happy path test

--- tests/unit-test/basic.ts
+++ tests/unit-test/basic.ts
@@ -1,6 +1,6 @@
 import path = require('path')
 import * as _ from 'lodash'
-import { localConductorConfig, installAgents, MEM_PROOF_BAD_SIG } from './utils'
+import { localConductorConfig, installAgents, INVALID_JOINING_CODE } from './utils'
 
 
 module.exports = async (orchestrator) => {
@@ -9,13 +9,13 @@ module.exports = async (orchestrator) => {
 
     let appInstallFailed;
     try{
-      await installAgents(conductor,  ["alice"], MEM_PROOF_BAD_SIG)
+      await installAgents(conductor, ["alice"], INVALID_JOINING_CODE)
       appInstallFailed = false
       console.error('Error: App intallation passed genesis with an invalid proof.')
       t.fail()
     } catch(e) {
       appInstallFailed = true
-      console.log(">>>>>>>>>>>> installAppResult",  e);
+      console.log("Install App Error: ",  e);
       t.deepEqual(e, {
         type: 'error',
         data: {
@@ -26,4 +26,34 @@ module.exports = async (orchestrator) => {
     }
     t.ok(appInstallFailed)
   })
+
+  // orchestrator.registerScenario('basic app functions', async (s, t) => {
+  //   // spawn the conductor process
+  //   const [ conductor ] = await s.players([localConductorConfig])
+
+  //   // test admin api endpoints: installation and activation
+  //   let installResult
+  //   try{
+  //     installResult = await installAgents(conductor,  ["alice"])
+  //     t.ok(installResult)
+  //   } catch(e) {
+  //     console.log("Error",  e)
+  //     t.fail()
+  //   }
+    
+  //   const [alice_test_happ] = installResult
+  //   const [alice] = alice_test_happ.cells
+
+  //   // test fn: returns_obj()
+  //   let response_object;
+  //   try{
+  //     response_object = await alice.call('test', 'returns_obj', null);
+  //     console.log("RESPONSE OBJECT:", response_object);
+  //     t.ok(response_object)
+  //     t.deep.equal(response_object, { value: "This is the returned value" })
+  //   } catch(e) {
+  //     console.error("Error: ", e);
+  //     t.fail()
+  //   }
+  // })
 }
--- tests/unit-test/utils.ts
+++ tests/unit-test/utils.ts
@@ -20,20 +20,17 @@ export const installation2agent: InstallAgentsHapps = [
   [[testDna]],
 ]
 
-// this mem_proof is a signature of the `{role, record_locator}` payload signing  a holo public key
-// The current holo_hosting pub key is `uhCAkfzycXcycd-OS6HQHvhTgeDVjlkFdE2-XHz-f_AC_5xelQX1N`
-const MEM_PROOF = Buffer.from("3gACrXNpZ25lZF9oZWFkZXLeAAKmaGVhZGVy3gACp2NvbnRlbnTeAAekdHlwZaZDcmVhdGWmYXV0aG9yxCeEICR/PJxdzJx345LodAe+FOB4NWOWQV0Tb5cfP5/8AL/nF6VBfU2pdGltZXN0YW1wks5gUzqazhJyV9WqaGVhZGVyX3NlcQmrcHJldl9oZWFkZXLEJ4QpJEIwak+vC8awMx0vdAe8XSbRRage/CuXmCjRhkkTtWWAUUOp8qplbnRyeV90eXBl3gABo0FwcN4AA6JpZACnem9tZV9pZACqdmlzaWJpbGl0ed4AAaZQdWJsaWPAqmVudHJ5X2hhc2jEJ4QhJAf4ZKktdaQZ6JJj4l+UDRCTwspZSchRPYXtwbdRVvyQBnB8ZqRoYXNoxCeEKSSebKOWLx1D9uHxPBkzVjOgm3gtO6w8VkiiEvigSfgTeFWLVN+pc2lnbmF0dXJlxEC+3INgyz2PfsiwtpBpTZIcx0JYVy9t7rYp2HWnK5x9Vw/uITWUzfIO4uaNl6MQppfkraxHLeNZqamjyEtRWggApWVudHJ53gABp1ByZXNlbnTeAAKqZW50cnlfdHlwZaNBcHClZW50cnnEMoKkcm9sZalkZXZlbG9wZXKucmVjb3JkX2xvY2F0b3Kybmljb2xhc0BsdWNrc3VzLmV1", 'base64')
-export const MEM_PROOF_BAD_SIG = Buffer.from("3gACrXNpZ25lZF9oZWFkZXLeAAKmaGVhZGVy3gACp2NvbnRlbnTeAAekdHlwZaZDcmVhdGWmYXV0aG9yxCeEICR/PJxdzJx345LodAe+FOB4NWOWQV0Tb5cfP5/8AL/nF6VBfU2pdGltZXN0YW1wks5gUzqazhJyV9WqaGVhZGVyX3NlcQmrcHJldl9oZWFkZXLEJ4QpJEIwak+vC8awMx0vdAe8XSbRRage/CuXmCjRhkkTtWWAUUOp8qplbnRyeV90eXBl3gABo0FwcN4AA6JpZACnem9tZV9pZACqdmlzaWJpbGl0ed4AAaZQdWJsaWPAqmVudHJ5X2hhc2jEJ4QhJAf4ZKktdaQZ6JJj4l+UDRCTwspZSchRPYXtwbdRVvyQBnB8ZqRoYXNoxCeEKSSebKOWLx1D9uHxPBkzVjOgm3gtO6w8VkiiEvigSfgTeFWLVN+pc2lnbmF0dXJlxEC+3INgyz2PfsiwtpBpTZIcx0JYVy9t7rYp2HWnK5x9Vw/uITWUzfIO4uaNl6MQppfkraxHLeNZqamjzEtRWggApWVudHJ53gABp1ByZXNlbnTeAAKqZW50cnlfdHlwZaNBcHClZW50cnnEMoKkcm9sZalkZXZlbG9wZXKucmVjb3JkX2xvY2F0b3Kybmljb2xhc0BsdWNrc3VzLmV1", 'base64')
+
+const SUCCESSFUL_JOINING_CODE = Buffer.from((msgpack.encode('joining code') as Buffer).toString('base64'), 'base64')
+// export const INVALID_JOINING_CODE = Buffer.from((msgpack.encode('failing joining code') as Buffer).toString('base64'), 'base64')
+export const INVALID_JOINING_CODE = Buffer.from("3gACrXNpZ25lZF9oZWFkZXLeAAKmaGVhZGVy3gA", 'base64')
 
 export const installAgents = async (conductor, agentNames, memProof?) => {
-  if (!memProof) {
-    memProof = MEM_PROOF
-  }
   const dnas = [
     {
       hash: await conductor.registerDna({path: testDna}),
       nick: 'elemental-chat',
-      membrane_proof: Array.from(memProof),
+      membrane_proof: Array.from(memProof || SUCCESSFUL_JOINING_CODE), // default to successful mem proof
       uid: conductor.scenarioUID,
     }
   ]
