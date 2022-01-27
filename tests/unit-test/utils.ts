55a98c955ae266a7a5c847cdcb6343e81cc40381
timotree3 <timorcb@gmail.com>
2022-01-27 16:46:12 -0500

    Use .happ file in tests so that multi-dna is tested

--- tests/unit-test/utils.ts
+++ tests/unit-test/utils.ts
@@ -1,47 +1,41 @@
-import { Orchestrator, Config, InstallAgentsHapps, InstalledHapp } from '@holochain/tryorama'
-import * as msgpack from '@msgpack/msgpack';
+import {
+  Config,
+  InstalledHapp,
+  Player
+} from '@holochain/tryorama'
+import * as msgpack from '@msgpack/msgpack'
 import * as path from 'path'
 
 export const delay = ms => new Promise(r => setTimeout(r, ms))
-export const wait = ms => new Promise((r, j)=>setTimeout(r, ms))
+export const wait = delay
 
 export const localConductorConfig = Config.gen()
 
-// Construct proper paths for your DNAs
-export const testDna = path.join(__dirname, "../../test.dna")
-
-// create an InstallAgentsHapps array with your DNAs to tell tryorama what
-// to install into the conductor.
-export const installation1agent: InstallAgentsHapps = [
-  [[testDna]],
-]
-export const installation2agent: InstallAgentsHapps = [
-  [[testDna]],
-  [[testDna]],
-]
+export const testHapp = path.join(__dirname, '../../test.happ')
 
 const SUCCESSFUL_JOINING_CODE = msgpack.encode('joining code')
 export const INVALID_JOINING_CODE = msgpack.encode('Failing Joining Code')
 
-export const installAgents = async (conductor, agentNames, memProof?) => {
-  const dnas = [
-    {
-      hash: await conductor.registerDna({path: testDna}),
-      role_id: 'elemental-chat',
-      membrane_proof: Array.from(memProof || SUCCESSFUL_JOINING_CODE), // default to successful mem proof
-      uid: conductor.scenarioUID,
-    }
-  ]
-  const admin = conductor.adminWs();
-  const agents: Array<InstalledHapp> = await Promise.all(agentNames.map(
-  async agent => {
+export const installAgents = async (
+  conductor: Player,
+  agentNames: string[],
+  memProof?: Uint8Array
+) => {
+  const admin = conductor.adminWs()
+  const agents: Array<InstalledHapp> = await Promise.all(
+    agentNames.map(async agent => {
       const req = {
         installed_app_id: `${agent}_chat`,
         agent_key: await admin.generateAgentPubKey(),
-        dnas
+        membrane_proofs: {
+          test: Buffer.from(memProof ? memProof : SUCCESSFUL_JOINING_CODE),
+          test2: Buffer.from(memProof ? memProof : SUCCESSFUL_JOINING_CODE),
+        },
+        uid: conductor.scenarioUID,
+        path: testHapp
       }
-      return await conductor._installHapp(req)
-    }
-  ))
+      return await conductor._installBundledHapp(req)
+    })
+  )
   return agents
 }
